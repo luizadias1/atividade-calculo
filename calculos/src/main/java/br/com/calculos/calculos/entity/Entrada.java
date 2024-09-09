593ce3688003ea152f9711b1eede4f7f0adc4429
luizadias1 <166713283+luizadias1@users.noreply.github.com>
2024-09-09 10:35:26 -0300

    Add files via upload

---
+++ calculos/src/main/java/br/com/calculos/calculos/entity/Entrada.java
@@ -0,0 +1,16 @@
+package br.com.calculos.calculos.entity;
+
+import java.util.List;
+
+
+public class Entrada {
+	private List<Integer> lista;
+	
+	public List<Integer> getLista(){
+		return lista;
+	}
+	
+	public void setLista(List<Integer> Lista) {
+		this.lista = lista;
+	}
+}
